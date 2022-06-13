da3791b813f72d8050026bdb9a3bd52e285b6fb5
Nahuel61920 <estebancarrizo619@gmail.com>
2022-06-13 16:43:24 -0300

    End 05-Bundlers

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,29 +1,27 @@
-(function () {
 
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
 
-  socket.on('connect', function () {
-    console.log('Connected!');
-  });
+const whiteboard = require('./whiteboard');
+const socket = require('socket.io-client');
 
-  socket.on('load', function (strokes) {
+socket.on('connect', function () {
+  console.log('Connected!');
+});
 
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
+socket.on('load', function (strokes) {
 
-  });
-
-  socket.on('draw', function (start, end, color) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
 
-  whiteboard.on('draw', function (start, end, color) {
-    socket.emit('draw', start, end, color);
-  });
+});
 
-})();
+socket.on('draw', function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
+
+whiteboard.on('draw', function (start, end, color) {
+  socket.emit('draw', start, end, color);
+});
